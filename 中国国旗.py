d6da18ab8451b1c26ae30646d78fb79cf7c00368
King-liangbaikai <65144290+King-liangbaikai@users.noreply.github.com>
2020-06-15 14:21:00 +0800

    Add files via upload

---
+++ 中国国旗.py
@@ -0,0 +1,64 @@
+import turtle
+
+turtle.bgcolor("red")
+turtle.fillcolor("yellow")
+turtle.color('yellow')
+turtle.speed(2)
+#主星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-600,220) 
+turtle.down()
+for i in range (5):    
+    turtle.forward(150)
+    turtle.right(144)
+turtle.end_fill()
+
+#第1颗副星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-400,295)
+turtle.setheading(305)
+turtle.down()
+for i in range (5):    
+    turtle.forward(50)
+    turtle.left(144)
+
+turtle.end_fill()
+
+
+#第2颗副星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-350,212)
+turtle.setheading(30)
+turtle.down()
+for i in range (5):  
+    turtle.forward(50)
+    turtle.right(144)
+
+turtle.end_fill()
+
+#第3颗副星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-350,145)
+turtle.setheading(5)
+turtle.down()
+for i in range (5):   
+    turtle.forward(50)
+    turtle.right(144)
+
+turtle.end_fill()
+
+#第4颗副星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-400,90)
+turtle.setheading(300)
+turtle.down()
+for i in range (5):  
+    turtle.forward(50)
+    turtle.left(144)
+
+turtle.end_fill()
